29efe272198b5ffc0ee1e7c23391b4683f8692ed
Gaikk <gaik.k02@gmail.com>
2025-11-09 15:22:03 +0100

    kinda goblin shooting

--- Assets/EnemyRangeController.cs
+++ Assets/EnemyRangeController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class EnemyRangeController : MonoBehaviour
@@ -12,7 +13,13 @@ public class EnemyRangeController : MonoBehaviour
 
     [Header("Ustawienia ataku")]
     public float attackRange = 2f;  
-    public float attackCooldown = 1.5f;
+    //public float attackCooldown = 1.5f;
+
+    [Header("Crossbow Settings")]
+    public GameObject crossbowArrowPrefab;
+    public Transform firePoint;
+    public float reloadTime = 3f;
+    public float arrowForce = 100f;
 
     [Header("Komponenty")]
     public Animator animator;
@@ -24,6 +31,10 @@ public class EnemyRangeController : MonoBehaviour
     private float attackTimer = 0f;
     private EnemyLife enemyLife;
 
+    private bool isMovementLocked = false;
+    private float movementLockTimer = 0f;
+
+
     private void Awake()
     {
         checkForPlayer = GetComponent<CheckForPlayer>();
@@ -33,72 +44,114 @@ public class EnemyRangeController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (!enemyLife.isAlive())
+        if (isMovementLocked)
         {
+            movementLockTimer -= Time.fixedDeltaTime;
+
+            velocity = Vector3.zero;
+            rb.MovePosition(rb.position);
+            animator.SetFloat("Velocity", 0f);
+
+            if (movementLockTimer <= 0f)
+                isMovementLocked = false;
+
             return;
-        }
-        if (!checkForPlayer.isTargetVisible)
-        {
-            velocity = Vector3.Lerp(velocity, Vector3.zero, Time.fixedDeltaTime * deceleration);
-            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
-            animator.SetFloat("Velocity", velocity.magnitude, 0.1f, Time.deltaTime);
-            return;
-        }
-
-        Transform target = checkForPlayer.targetGameObject.transform;
-        float distanceToTarget = Vector3.Distance(transform.position, target.position);
-
-        Vector3 lookDir = (target.position - transform.position);
-        lookDir.y = 0f;
-
-        if (rotateTowardsMoveDirection && velocity.sqrMagnitude > 0.01f)
-        {
-            Quaternion targetRot = Quaternion.LookRotation(velocity.normalized, Vector3.up);
-            rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRot, Time.fixedDeltaTime * 10f));
-        }
-
-        if (attackTimer > 0f)
-            attackTimer -= Time.fixedDeltaTime;
-
-        if (distanceToTarget > attackRange)
-        {
-            inputDir = lookDir.normalized;
-            // Wyznaczenie docelowej prêdkoœci
-            Vector3 targetVelocity = inputDir * moveSpeed;
-            velocity = Vector3.Lerp(velocity, targetVelocity, Time.fixedDeltaTime * (inputDir.magnitude > 0 ? acceleration : deceleration));
-
-            // Ruch
-            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
-
-            // Rotacja w kierunku ruchu
-            animator.SetFloat("Velocity", velocity.magnitude, 0.1f, Time.deltaTime);
-            animator.ResetTrigger("Melee1");
-            animator.ResetTrigger("Melee2");
         }
         else
         {
-            velocity = Vector3.Lerp(velocity, Vector3.zero, Time.fixedDeltaTime * deceleration);
-            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
-            animator.SetFloat("Velocity", 0f);
-
-            if (attackTimer <= 0f)
+            if (!enemyLife.isAlive())
             {
-                Attack();
-                attackTimer = attackCooldown;
+                return;
             }
+            if (!checkForPlayer.isTargetVisible)
+            {
+                velocity = Vector3.Lerp(velocity, Vector3.zero, Time.fixedDeltaTime * deceleration);
+                rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+                animator.SetFloat("Velocity", velocity.magnitude, 0.1f, Time.deltaTime);
+                return;
+            }
+
+            Transform target = checkForPlayer.targetGameObject.transform;
+            float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+            Vector3 lookDir = (target.position - transform.position);
+            lookDir.y = 0f;
+
+            if (rotateTowardsMoveDirection && velocity.sqrMagnitude > 0.01f)
+            {
+                Quaternion targetRot = Quaternion.LookRotation(velocity.normalized, Vector3.up);
+                rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRot, Time.fixedDeltaTime * 10f));
+            }
+
+            if (attackTimer > 0f)
+                attackTimer -= Time.fixedDeltaTime;
+
+            if (distanceToTarget > attackRange)
+            {
+                inputDir = lookDir.normalized;
+                // Wyznaczenie docelowej prêdkoœci
+                Vector3 targetVelocity = inputDir * moveSpeed;
+                velocity = Vector3.Lerp(velocity, targetVelocity, Time.fixedDeltaTime * (inputDir.magnitude > 0 ? acceleration : deceleration));
+
+                // Ruch
+                rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+
+                // Rotacja w kierunku ruchu
+                //animator.SetFloat("Velocity", velocity.magnitude, 0.1f, Time.deltaTime);
+                animator.SetBool("IsAiming", false);
+            }
+            else
+            {
+                velocity = Vector3.Lerp(velocity, Vector3.zero, Time.fixedDeltaTime * deceleration);
+                rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+                animator.SetBool("IsAiming", true);
+
+                //Vector3 forward = transform.forward;
+                Vector3 toTarget = (target.position - transform.position).normalized;
+
+                Quaternion lookRot = Quaternion.LookRotation(toTarget, Vector3.up);
+
+                Quaternion offset = Quaternion.Euler(0f, 36f, 0f);
+
+                Quaternion finalRot = lookRot * offset;
+                rb.MoveRotation(Quaternion.Slerp(rb.rotation, finalRot, Time.fixedDeltaTime * 10f));
+
+
+                Vector3 newForward = rb.rotation * Vector3.forward;
+                float dot = Vector3.Dot(newForward, toTarget);
+
+                bool isFacingTarget = dot > 0.8f;
+
+                if (isFacingTarget && attackTimer <= 0f)
+                {
+                    UnityEngine.Debug.Log($"attack");
+                    Attack();
+                    attackTimer = reloadTime;
+                }
+                UnityEngine.Debug.Log($"Dist={distanceToTarget:F2} isFacingTarget={isFacingTarget} attacktimer={attackTimer}");
+            }
+
+            animator.SetFloat("Velocity", velocity.magnitude, 0.1f, Time.deltaTime);
+
         }
     }
 
     private void Attack()
     {
-        int attackIndex = UnityEngine.Random.Range(0, 2);
-        string triggerName = attackIndex == 0 ? "Melee1" : "Melee2";
+        isMovementLocked = true;
+        movementLockTimer = 2f;
 
-        animator.ResetTrigger("Melee1");
-        animator.ResetTrigger("Melee2");
+        animator.ResetTrigger("Reload");
+        animator.SetBool("IsAiming", false);
 
-        animator.SetTrigger(triggerName);
+        animator.SetTrigger("Reload");
 
-        // TODO logika napierdalania playera po ryju
+        if (crossbowArrowPrefab != null && firePoint != null)
+        {
+            GameObject arrow = UnityEngine.Object.Instantiate(crossbowArrowPrefab, firePoint.position, firePoint.rotation);
+            Rigidbody rb = arrow.GetComponent<Rigidbody>();
+            if (rb != null)
+                rb.AddRelativeForce(Vector3.forward * arrowForce, ForceMode.Impulse);
+        }
     }
 }
