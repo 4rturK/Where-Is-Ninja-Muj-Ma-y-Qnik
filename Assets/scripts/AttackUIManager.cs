efee64ed38c984a50f216667a7046794f707a93e
SzymonCieslar24 <cieslarszymon24@gmail.com>
2025-12-06 23:24:09 +0100

    Added basic start UI with option to choose the microphone.

--- Assets/scripts/AttackUIManager.cs
+++ Assets/scripts/AttackUIManager.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using static UnityEditor.Progress;
 
 public class AttackUIManager : MonoBehaviour
 {
@@ -10,11 +13,34 @@ public class AttackUIManager : MonoBehaviour
     public TMP_Dropdown rightDropdown;
     public TMP_Dropdown add1Dropdown;
     public TMP_Dropdown add2Dropdown;
+    public TMP_Dropdown micDropdown;
 
+    public GameObject startPanel;
     public GameObject attackSelectPanel;
     public GameObject gamePanel;
 
+    public Button startButton;
+
     public ManagerGame gameManager;
+    //public MicrophoneInput micInput;
+
+    public bool pauseOnStart = true;
+
+    private List<string> _options = new List<string>();
+    private const string PlayerPrefsKey = "mic_device";
+
+    private void Awake()
+    {
+        if (pauseOnStart)
+        {
+            Time.timeScale = 0f;
+        }
+
+        if (startPanel != null) startPanel.SetActive(true);
+
+        if (gamePanel != null) gamePanel.SetActive(false);
+        if (attackSelectPanel != null) attackSelectPanel.SetActive(false);
+    }
 
     void Start()
     {
@@ -26,15 +52,22 @@ public class AttackUIManager : MonoBehaviour
 
         //Debug.Log(attackNames.Count);
 
+        var devices = Microphone.devices;
+        _options = new List<string>(devices);
+
         SetupDropdown(leftDropdown, attackNames, attacksManager.leftAttack);
         SetupDropdown(rightDropdown, attackNames, attacksManager.rightAttack);
         SetupDropdown(add1Dropdown, attackNames, attacksManager.add1Attack);
         SetupDropdown(add2Dropdown, attackNames, attacksManager.add2Attack);
+        SetupDropdown(micDropdown, _options, attacksManager.add2Attack);
 
         leftDropdown.onValueChanged.AddListener(OnLeftChanged);
         rightDropdown.onValueChanged.AddListener(OnRightChanged);
         add1Dropdown.onValueChanged.AddListener(OnAdd1Changed);
         add2Dropdown.onValueChanged.AddListener(OnAdd2Changed);
+        micDropdown.onValueChanged.AddListener(OnMicChanged);
+
+        startButton.onClick.AddListener(OnStartClicked);
     }
 
     private void Update()
@@ -118,5 +151,36 @@ public class AttackUIManager : MonoBehaviour
         else
             attacksManager.add2Attack = null;
         attacksManager.ApplySelectedAttacks();
+    }
+
+    private void OnMicChanged(int index)
+    {
+        if (_options == null || _options.Count == 0) return;
+        index = Mathf.Clamp(index, 0, _options.Count - 1);
+        string dev = _options[index];
+
+        // Zapisz wybór
+        PlayerPrefs.SetString(PlayerPrefsKey, dev);
+        PlayerPrefs.Save();
+
+        // Przeka¿ do MicrophoneInput
+        //if (micInput != null)
+        //{
+        //    micInput.SetDevice(dev);
+        //}
+        //else
+        //{
+        //    Debug.LogWarning($"StartMenuUI: Brak referencji do MicrophoneInput. Wybrane urz¹dzenie: {dev}");
+        //}
+    }
+
+    private void OnStartClicked()
+    {
+        Debug.Log("StartMenuUI: klikniêto 'Graj'.");
+
+        if (startPanel) startPanel.SetActive(false);
+        if (gamePanel) gamePanel.SetActive(true);
+
+        Time.timeScale = 1f;
     }
 }
